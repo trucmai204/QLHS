c93467d1ff8477c32d8faf545eca515b2a1d40de
Phan Xuân Quang <20521008@ms.uit.edu.vn>
2024-06-02 11:46:33 +0700

    init

---
+++ Entities/Student.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities
+{
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; }
+        public bool Gender { get; set; }
+        public int ClassId { get; set; }
+
+        [ForeignKey("ClassId")]
+        public virtual Class Class { get; set; }
+    }
+}
---
+++ Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
